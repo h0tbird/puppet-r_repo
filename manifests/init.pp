aef7cc7f9887513aebc4704b234d0b770c42724e
Marc Villacorta <marc.villacorta@gmail.com>
2014-09-07 21:30:46 +0200

    Apache setup

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,12 @@
 class r_repo {
 
+  class { 'apache':
+    default_mods        => false,
+    default_confd_files => false,
+  }
+
+  apache::vhost { 'repo.demo.lan':
+    port    => '80',
+    docroot => '/var/www/repos',
+  }
 }
