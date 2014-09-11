ccf2165337f7d4aa1b90607594e88f09f1b45ba3
Marc Villacorta <marc.villacorta@gmail.com>
2014-09-11 13:53:44 +0200

    Set apache defaults

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,4 @@
 class r_repo {
 
-  class { 'apache':
-    default_mods        => false,
-    default_confd_files => false,
-  }
-
-  apache::vhost { 'repo.demo.lan':
-    port    => '80',
-    docroot => '/var/www/repos',
-  }
+  class { 'apache': }
 }
